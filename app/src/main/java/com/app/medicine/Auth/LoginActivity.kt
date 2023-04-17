35cfcb8dcfcbb0db349563e7b72c53708c978907
HoangNam <namtran0126@gmail.com>
2023-04-17 23:11:52 +0700

    fragment main view

--- app/src/main/java/com/app/medicine/Auth/LoginActivity.kt
+++ app/src/main/java/com/app/medicine/Auth/LoginActivity.kt
@@ -2,7 +2,9 @@ package com.app.medicine.Auth
 
 import android.app.ProgressDialog
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+//import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 import android.util.Log
 import com.app.medicine.API.Api
--- app/src/main/java/com/app/medicine/MainActivity.kt
+++ app/src/main/java/com/app/medicine/MainActivity.kt
@@ -2,9 +2,21 @@ package com.app.medicine
 
 import com.app.medicine.Auth.LoginActivity
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+//import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.core.view.GravityCompat
+import androidx.navigation.Navigation;
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.ui.NavigationUI
+import com.app.medicine.views.HomeActivity
+import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.nav_drawer.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +24,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnStart.setOnClickListener() {
-            val i = Intent(this, LoginActivity::class.java)
+            val i = Intent(this, HomeActivity::class.java)
             startActivity(i)
         }
+
+
+
     }
 
 }
